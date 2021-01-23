222fa9c9779d6161c9436d9dd23777685087f711
Q118 <shelbyfish91@gmail.com>
2021-01-23 12:15:48 -0800

    initializing first react context

---
+++ context/cart.js
@@ -0,0 +1,20 @@
+/** @format */
+
+import React, { createContext, useReducer } from "react";
+
+const initialState = [];
+
+function reducer(state, action) {
+	//...
+}
+
+export const CartContext = createContext();
+
+const CartProvider = ({ children }) => {
+	const [state, dispatch] = useReducer(reducer, initialState);
+	return (
+		<CartContext.Provider value={{ state, dispatch }}>
+			{children}
+		</CartContext.Provider>
+	);
+};
