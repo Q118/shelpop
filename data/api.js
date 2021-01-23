53a3e5f1bb9022d32227b2f461692f0a16f13eed
Q118 <shelbyfish91@gmail.com>
2021-01-23 12:05:30 -0800

    get products functionality

--- data/api.js
+++ data/api.js
@@ -3,9 +3,16 @@
 import json from "./products.json";
 
 export function getProducts() {
-	const products = getProducts();
+    return json.products;    
+}
+ 
 
+export async function withProducts() {
+    const products = getProducts();
+//withProducts is an asynch function so we could also execute code that takes time to execute; such as fetching data with a promise-based API: 
+    //const products = await fetch('https://...');
 	return {
 		products,
 	};
 }
+
