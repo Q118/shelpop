c47935216720514f1214db21c7151296c4353e57
Q118 <shelbyfish91@gmail.com>
2021-01-31 09:09:26 -0800

    style sheets rendered

--- data/api.js
+++ data/api.js
@@ -1,18 +1,22 @@
-/** @format */
-
 import json from "./products.json";
 
 export function getProducts() {
-    return json.products;    
+  return json.products;
 }
- 
 
 export async function withProducts() {
-    const products = getProducts();
-//withProducts is an asynch function so we could also execute code that takes time to execute; such as fetching data with a promise-based API: 
-    //const products = await fetch('https://...');
-	return {
-		products,
-	};
+  const products = getProducts();
+  return {
+    products,
+  };
 }
 
+export const getTotalSum = (products, items) => {
+  return items
+    .map(({ id, amount }) => ({
+      product: products.find((p) => p.id === id),
+      amount,
+    }))
+    .map(({ product: { price }, amount }) => amount * price)
+    .reduce((acc, cur) => acc + cur, 0);
+};
